c1b53b4d5a1e393df4080b65c526d4d07786bdd9
Jackson Wozniak <jjw30@comcast.net>
2022-06-05 13:13:55 -0400

    added notNull test for setChart class

--- src/test/java/Main/java/setChartTest.java
+++ src/test/java/Main/java/setChartTest.java
@@ -1,7 +1,19 @@
 package Main.java;
 
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class setChartTest {
+
+    @Test
+    void checkJPanelSize(){
+        int[] years = {1,2,3,4};
+        double[] values = {1,2,3,4};
+        JPanel panel = new setChart(years, values);
+        assertNotNull(panel);
+    }
 
 }
